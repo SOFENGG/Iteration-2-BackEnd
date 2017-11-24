c591e3115bdf27e9a1f3263b36ad13c01282ecb2
Luigi <luigi_acorda@dlsu.edu.ph>
2017-11-24 21:38:42 +0800

    sql

--- autosupply.sql
+++ autosupply.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.20, for Win64 (x86_64)
 --
 -- Host: localhost    Database: autosupply
 -- ------------------------------------------------------
--- Server version	5.7.19-log
+-- Server version	5.7.20-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -76,7 +76,7 @@ CREATE TABLE `items` (
 
 LOCK TABLES `items` WRITE;
 /*!40000 ALTER TABLE `items` DISABLE KEYS */;
-INSERT INTO `items` VALUES ('0','white wheel','they see me rollin','Wheel','Toyota',1,2,0,'1998-11-10',20.0000,25.0000),('1','black tinted window','they hatin','Window','Honda',0,4,0,'2017-11-10',20.0000,25.0000);
+INSERT INTO `items` VALUES ('0','white wheel','they see me rollin','Wheel','Toyota',1,2,2,'1998-11-10',20.0000,25.0000),('1','black tinted window','they hatin','Window','Honda',0,2,1,'2017-11-10',20.0000,25.0000);
 /*!40000 ALTER TABLE `items` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -88,14 +88,14 @@ DROP TABLE IF EXISTS `items_log`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `items_log` (
-  `sale_id` int(11) NOT NULL,
+  `sale_id` int(11) NOT NULL AUTO_INCREMENT,
   `item_code` varchar(45) NOT NULL,
   `transaction_id` int(11) NOT NULL,
   `quantity_sold` int(11) NOT NULL,
   `original_price` decimal(19,4) NOT NULL,
   `price_sold` decimal(19,4) NOT NULL,
   PRIMARY KEY (`sale_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -104,6 +104,7 @@ CREATE TABLE `items_log` (
 
 LOCK TABLES `items_log` WRITE;
 /*!40000 ALTER TABLE `items_log` DISABLE KEYS */;
+INSERT INTO `items_log` VALUES (1,'1',1,2,50.0000,20.0000);
 /*!40000 ALTER TABLE `items_log` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -208,6 +209,7 @@ CREATE TABLE `transactions` (
 
 LOCK TABLES `transactions` WRITE;
 /*!40000 ALTER TABLE `transactions` DISABLE KEYS */;
+INSERT INTO `transactions` VALUES (1,4,'RETAIL SALE',0,'2017-11-24',10.0000);
 /*!40000 ALTER TABLE `transactions` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -273,4 +275,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-11-23  9:59:22
+-- Dump completed on 2017-11-24 21:37:22
